49a20ab21854c185e2d3981a42f69b96ab81fed2
Lucas Tiago <eulukasthyago@outlook.com>
2024-06-09 20:24:26 -0300

    :iphone: Work on responsive design.

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,10 +1,41 @@
 {
+
+  if (document.querySelector(".header_search_mobile_action_button")) {
+    const toggle_search = document.querySelector(".header_search_mobile_action_button");
+    toggle_search.addEventListener('click', () => {
+      if (document.querySelector(".mobile_search")) {
+        const mobile_search = document.querySelector(".mobile_search");
+        mobile_search.classList.toggle('show');
+      }
+    });
+  }
+
+  if (document.querySelector(".header_menu_mobile_action_open") && document.querySelector(".mobile_clode_menu")) {
+    const open_menu = document.querySelector(".header_menu_mobile_action_open");
+    const close_menu = document.querySelector(".mobile_clode_menu");
+    open_menu.addEventListener("click", () => {
+      if (document.querySelector(".header_menu_nav")) {
+        const mobile_search = document.querySelector(".header_menu_nav");
+        mobile_search.classList.add("show");
+      }
+    });
+    close_menu.addEventListener("click", () => {
+      if (document.querySelector(".header_menu_nav")) {
+        const mobile_search = document.querySelector(".header_menu_nav");
+        mobile_search.classList.remove("show");
+      }
+    });
+  }
+
   new Splide(".splide", {
     type: "loop",
     perPage: 4,
-    // gap: "8px",
     autoWidth: true,
     pagination: false,
-    // padding: '100px'
+    breakpoints: {
+      640: {
+        perPage: 2,
+      },
+    },
   }).mount();
 }
